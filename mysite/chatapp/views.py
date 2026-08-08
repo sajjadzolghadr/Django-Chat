1c2be9ada4bd4f0fc1c509adc366dced1ca67315
sajjad <sajjadzolghadr777@gmail.com>
2026-08-08 13:03:25 +0330

    feat:add chatrooms to view and index page

--- mysite/chatapp/views.py
+++ mysite/chatapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import ChatRoom
 # Create your views here.
 def index(request):
-    return render(request, 'chatapp/index.html')
+    chat_rooms = ChatRoom.objects.all()
+    return render(request, 'chatapp/index.html', {'chat_rooms': chat_rooms})
