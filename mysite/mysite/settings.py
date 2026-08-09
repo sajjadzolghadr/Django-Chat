f6c56675e9ab3a7f9ae43c42880b8a0ab1b7b16d
sajjad <sajjadzolghadr777@gmail.com>
2026-08-09 12:13:14 +0330

    feat:Create a Consumer

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -71,6 +71,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 ASGI_APPLICATION = 'mysite.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    }
+}
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
