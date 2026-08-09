476074713a241ea171e6969a2c8ab8cbc1a7ffd8
sajjad <sajjadzolghadr777@gmail.com>
2026-08-09 11:20:19 +0330

    feat:add chatroom view and room page

--- mysite/chatapp/views.py
+++ mysite/chatapp/views.py
@@ -4,3 +4,7 @@ from .models import ChatRoom
 def index(request):
     chat_rooms = ChatRoom.objects.all()
     return render(request, 'chatapp/index.html', {'chat_rooms': chat_rooms})
+
+def chatroom(request,slug):
+    chatroom = ChatRoom.objects.get(slug=slug)
+    return render(request, 'chatapp/room.html', {'chatroom': chatroom})
